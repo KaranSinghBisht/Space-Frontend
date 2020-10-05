e3312b978183e847955ff046e6ac6cd98eb22889
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-05 13:45:12 +0800

    Add IconLink component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import './styles/variables.css'
 import './App.css'
-import { RegisterNewsletter } from './components/RegisterNewsletter'
 
 function App() {
   return <div className="App"></div>
