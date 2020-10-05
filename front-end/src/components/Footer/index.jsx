47c308f386aec9e654a870fb46261065e650b642
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-05 10:45:36 +0800

    Add css variables

--- front-end/src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <footer>
-      <h1>dis is the footer</h1>
-    </footer>
-  )
-}
---
+++ front-end/src/components/Icon/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+import styles from './Icon.module.css'
+
+export const Icon = () => {
+  return <div className={styles.container}>ICON</div>
+}
