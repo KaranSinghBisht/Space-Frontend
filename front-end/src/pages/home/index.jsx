923b87bdf07f0fd66ca59e281db3824e2b2e1927
El Mehdi Boumhicha <96.elmehdi@gmail.com>
2020-10-06 21:07:17 +0200

    Improve syntax style of calling the Component

--- front-end/src/pages/home/index.jsx
+++ front-end/src/pages/home/index.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 import { DefaultLayout } from '../../layouts/DefaultLayout'
 
-export const Home = () => {
-  return (
-    <DefaultLayout>
-      <p>This will be the home page</p>
-    </DefaultLayout>
-  )
-}
+export const Home = () => (
+  <DefaultLayout>
+    <p>This will be the home page</p>
+  </DefaultLayout>
+);
