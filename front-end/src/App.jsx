00b160bbebe86149f97d8c887fb562f316c2e912
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-06 10:05:18 +0800

    Add router set up for dummy pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
-import Navs from './Nav/Nav'
+import { Route, Switch } from 'react-router-dom'
 
 import './styles/variables.css'
 import './App.css'
+import { Home } from './pages/home'
+import { Forum } from './pages/forum'
+import { Explore } from './pages/explore'
 
-export const App = () => {
+function App() {
   return (
     <div className="App">
-      <Navs />
+      <Switch>
+        <Route exact path="/explore" component={Explore} />
+        <Route exact path="/forum" component={Forum} />
+        <Route path="/" component={Home} />
+      </Switch>
     </div>
   )
 }
+
+export default App
--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './App'
+
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
