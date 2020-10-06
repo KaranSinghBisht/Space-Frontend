fa317d38cf8e15fec4432079517ec20601ac6074
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-10-06 10:01:14 +0800

    Add dummy pages

---
+++ front-end/src/pages/forum/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { DefaultLayout } from '../../layouts/DefaultLayout'
+
+export const Forum = () => {
+  return (
+    <DefaultLayout>
+      <p>This will be the forum page</p>
+    </DefaultLayout>
+  )
+}
